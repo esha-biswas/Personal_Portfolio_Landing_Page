1c6d78a852495f1624628c6fd99d2409904fb330
Esha <eshabiswas000@gmail.com>
2024-08-16 09:38:24 +0530

    updated next.config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 const nextConfig = {
    basePath: "/Personal_Portfolio_Landing_Page",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
+
+
 
 export default nextConfig;
