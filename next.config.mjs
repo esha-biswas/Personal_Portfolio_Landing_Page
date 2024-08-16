0197e260a967a750f4735fc37676932007a73b95
Esha <eshabiswas000@gmail.com>
2024-08-16 09:53:57 +0530

    updated next.config.mjs again

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-/* @type {import('next').NextConfig} */
+/**  @type {import('next').NextConfig} */
 const nextConfig = {
   //  basePath: "/Personal_Portfolio_Landing_Page",
   output: "export",  // <=== enables static exports
